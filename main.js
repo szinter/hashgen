b60964b917e4538a93f732af940050d91aca4969
Attila Gal <attilagal@Attila-Gal-mbp.local>
2019-02-26 18:08:29 +0100

    Add sentry

--- main.js
+++ main.js
@@ -1,4 +1,10 @@
 const { Passgen } = require('./passgen');
+const Sentry = require('@sentry/node');
+
+Sentry.init({
+    dsn: 'https://d373da4f59714d0db5b1f832d32dc323@sentry.io/1403188',
+    release: 'hashgen@0.1.2'
+});
 
 const passgen = new Passgen();
 const ITERATIONS = 30;
